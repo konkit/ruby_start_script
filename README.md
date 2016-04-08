e6edb8f76221e99de0b6d31ebb932ae6babc3665
Lukasz Tenerowicz <konkit@gmail.com>
2016-04-08 21:03:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+= Ruby Start Script
+
+Script for basic provisioning of the Rails project - so e.g. when scaffolding, are generators will be already set
---
+++ ruby_start_script.sh
@@ -0,0 +1,23 @@
+gem install gemrat
+
+gemrat haml-rails
+rails generate haml:application_layout convert
+rake haml:erb2haml
+
+gemrat bootstrap-sass
+gemrat bootstrap-generators
+rails generate bootstrap:install
+
+gemrat simple_form
+rails generate simple_form:install --bootstrap
+
+gemrat shoulda-matchers
+
+gemrat rspec-rails
+rails g rspec:install
+
+gemrat factory_girl
+
+bundle
+
+
